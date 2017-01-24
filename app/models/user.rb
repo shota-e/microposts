1ca65ce4439c6a73e4e6a9060eeb3eca7f611cd3
shota101207 <ebi101207@yahoo.co.jp>
2017-01-24 05:44:54 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,22 @@ class User < ActiveRecord::Base
     validates :area, presence: true,allow_blank: true
     has_secure_password
     has_many :microposts
+    
+    has_many :following_relationships, class_name:  "Relationship",
+                                       foreign_key: "follower_id",
+                                       dependent:   :destroy
+    has_many :following_users, through: :following_relationships, source: :followed                                   
+
+    def follow(other_user)
+        following_relationships.find_or_create_by(followed_id: other_user.id)
+    end
+    
+    def unfollow(other_user)
+        following_relationship = following_relationships.find_by(followed_id: other_user.id)
+        following_relationship.destroy if following_relationship
+    end
+    
+    def following?(other_user)
+        following_users.include?(other_user)
+    end
 end
